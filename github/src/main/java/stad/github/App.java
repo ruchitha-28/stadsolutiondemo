06a8f27e3d6899904ee3fee3079b6d697a3a8a99
HP <HP@Naresh.lan>
2026-03-17 17:00:54 +0530

    demo class is added

---
+++ github/src/main/java/stad/github/App.java
@@ -0,0 +1,10 @@
+package stad.github;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ github/src/test/java/day1practice/staddemo.java
@@ -0,0 +1,10 @@
+package day1practice;
+
+public class staddemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("this a demo class");
+	}
+
+}

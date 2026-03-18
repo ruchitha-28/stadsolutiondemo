0133bd5e7512e57d4b6e518e76ec1bba1ea530f6
HP <HP@Naresh.lan>
2026-03-18 16:20:57 +0530

    i have done practice

--- github/src/test/java/practice2.java
+++ github/src/test/java/practice2.java
@@ -1,9 +1,9 @@
 
-public class test3 {
+public class practice2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("this is my test3");
+   System.out.println("it is a practice1");
 	}
 
 }
